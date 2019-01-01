a438b3f1deeb9440908ae79e6b84e186bdbc4d6e
Akshit Patel <akshitpatel72@gmail.com>
2019-01-01 18:14:36 +0530

    Indented

--- B/B5.js
+++ B/B5.js
@@ -1,8 +1,9 @@
 var t=setInterval(function(){
 	var a = document.getElementsByTagName('img');
-for(var i=0;i< a.length;i++){
-	if((a[i].getBoundingClientRect().top < 754 && a[i].getBoundingClientRect().top > 0)  || (a[i].getBoundingClientRect().bottom >=0 && a[i].getBoundingClientRect().bottom  < 754)){
-	a[i].style.animation = "animate 0.25s forwards";
-}
-}
+
+	for(var i=0 ; i < a.length ; i++){
+		if( ( a[i].getBoundingClientRect().top < 754 && a[i].getBoundingClientRect().top > 0 )  || ( a[i].getBoundingClientRect().bottom >= 0 && a[i].getBoundingClientRect().bottom < 754 ) ){
+			a[i].style.animation = "animate 0.25s forwards";
+		}
+	}
 },1);
