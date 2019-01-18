9be88f72f8824c9fea50641eb37b0ec50f2c7998
Akshit Patel <akshitpatel72@gmail.com>
2019-01-18 17:52:03 +0530

    B10 completed

--- B/B10.js
+++ B/B10.js
@@ -3,24 +3,51 @@ var profilePhoto = document.getElementById("profilePhoto");
 var bio = document.getElementById("bio");
 var profileName = document.getElementById("name");
 
-function XML_HTTP(){
-var input = document.querySelector("input").value;
-console.log("HI");
-var request = new XMLHttpRequest();
-
-request.open('GET', `https://api.github.com/users/${input}`, true);
-request.onload = function () {
-
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-   profilePhoto.style.backgroundImage = `url("${data.avatar_url}")`;
-   profileName.innerHTML = `${data.name}`;
-   bio.innerHTML = `${data.bio}`;
-  } else {
-    console.log('error');
-  }
+function represent_data(data){
+	profilePhoto.style.backgroundImage = `url("${data.avatar_url}")`;
+	profileName.innerHTML = `${data.name}`;
+	bio.innerHTML = `${data.bio}`;	
 }
 
+function XML_HTTP(){
+
+var input = document.querySelector("input").value;
+var request = new XMLHttpRequest();
+request.open('GET', `https://api.github.com/users/${input}`, true);
+
+request.onload = function () {
+	var data = JSON.parse(this.response);
+	if (request.status >= 200 && request.status < 400) {
+		represent_data(data);
+	} else {
+		console.log('error');
+	}
+}
 request.send();
+
+}
+
+function fetch_API(){
+
+var input = document.querySelector("input").value;
+fetch(`https://api.github.com/users/${input}`)
+  .then((response) =>response.json() /*function(response) => response.json()*/)
+  .then(function(data){
+    represent_data(data);  
+  })
+  .catch(err => console.error(err));
+
+}
+
+function jQuery_AJAX(){
+
+var input = document.querySelector("input").value;
+$.ajax({
+    url: `https://api.github.com/users/${input}`,
+    type: 'GET',
+    success: function(data) {
+      represent_data(data);
+    }
+});
+
 }
